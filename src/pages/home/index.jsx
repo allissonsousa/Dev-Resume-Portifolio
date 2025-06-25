4ed21ed7c688d6cd7cc0c30beedc6fc64b1fdd56
Allison de Paulo Sousa Avila <111160403+allissonsousa@users.noreply.github.com>
2025-06-24 23:28:08 -0300

    hmrupdate

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,30 @@
 import { useState } from 'react'
 import './style.css'
+import Trash from '../../assets/excluir.png'
+
 
 function Home() {
   const [count, setCount] = useState(0)
 
+  const users = [{
+    id: '23123h12hj21',
+    name: 'Pedro',
+    age: 23,
+    email: 'pedro@gmail.com'
+  },
+  {
+    id: '23123h12hjaas1',
+    name: 'Marcos',
+    age: 65,
+    email: 'marco23@gmail.com'
+  },
+  {
+    id: '23123h12h12fsas1',
+    name: 'Marcela',
+    age: 35,
+    email: 'marcela23@gmail.com'
+  }
+  ]
   return (
 
     <div className='container'>
@@ -12,19 +33,23 @@ function Home() {
         <input type="text" name="nome" id="" />
         <input type="number" name="idade" id="" />
         <input type="email" name="email" id="" />
-        <button>Cadastrar</button>
+        <button type='button'>Cadastrar</button>
       </form>
-      
-      <div>
-        <div>
-          <p>Nome: </p>
-          <p>Idade: </p>
-          <p>E-mail: </p>
+
+      {users.map(user => (
+        <div key={user.id}>
+          <div>
+            <p>Nome: {user.name}</p>
+            <p>Idade: {user.age}</p>
+            <p>E-mail: {user.email}</p>
+          </div>
+          <button>
+            <img src={Trash} />
+          </button>
         </div>
-        <button><img src="" alt="" /></button>
-        <div></div>
-      </div>
+      ))}
     </div>
+
   )
 }
 export default Home
