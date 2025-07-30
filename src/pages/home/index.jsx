e2307c6a8e8325d8cd8e50a7efe558b1aa4bc353
Allison de Paulo Sousa Avila <allissonsousa7@outlook.com>
2025-07-30 15:26:48 -0300

    Add new asset images and update home page layout
    Added several new image assets (discord, github, linkedin, mail, pontos, quadrado, simbol, telegram) and removed the excluir.png asset. Updated the home page by introducing navegcao and describe components, and removed the placeholder navigation element.

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,12 +2,29 @@ import { useState,useEffect } from 'react'
 import './style.css'
 import Trash from '../../assets/excluir.png'
 
+function navegcao(){
+  const navega = (<header>
+    <p>Allisson Sousa</p>
+    <ul>
+      <li>#home</li>
+      <li>#trabalhos</li>
+      <li>#sobre mim</li>
+      <li>#contate</li>
+    </ul>
+  </header>);
+  return navega;
+}
+
+function describe(){
+  return (
+    <h1></h1>
+  )
+}
 
 function Home() {
   const home = useState(false)
   return (
     <body>
-      <nav>navegação</nav>
       <aside>nome e foto</aside>
       <p>slogan</p>
       <article >projetos
