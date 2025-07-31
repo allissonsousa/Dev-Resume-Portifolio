46202b918756a572020e5c432c181aead132be08
Allison de Paulo Sousa Avila <allissonsousa7@outlook.com>
2025-07-31 16:41:31 -0300

    Refactor home page layout and project display
    Refactored the home page by renaming functions for clarity, updating the navigation component, and introducing a dynamic project display section with images. Improved code formatting and structure for better readability and maintainability.

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,42 +1,56 @@
-import { useState,useEffect } from 'react'
-import './style.css'
-import Trash from '../../assets/excluir.png'
+import { useState, useEffect } from "react";
+import "./style.css";
+import discord from "./assets/discord.png";
 
-function navegcao(){
-  const navega = (<header>
-    <p>Allisson Sousa</p>
-    <ul>
-      <li>#home</li>
-      <li>#trabalhos</li>
-      <li>#sobre mim</li>
-      <li>#contate</li>
-    </ul>
-  </header>);
-  return navega;
-}
-
-function describe(){
+function navegacao() {
   return (
-    <h1></h1>
-  )
+    <header>
+      <p>Allisson Sousa</p>
+      <ul>
+        <li>#home</li>
+        <li>#trabalhos</li>
+        <li>#sobre mim</li>
+        <li>#contate</li>
+      </ul>
+    </header>
+  );
 }
 
+function exibeprojeto() {
+  const projetos = [
+    { id: 1, nome: "python", simbolo: discord },
+    { id: 2, nome: "java", simbolo: "./assets/java.jpg" },
+    { id: 3, nome: "web", simbolo: "./assets/java.jpg" },
+    { id: 4, nome: "react", simbolo: "./assets/java.jpg" },
+  ];
+  return (
+    <div style={{ display: "flex", flexdirection: "row" }}>
+      {projetos.map((projeto) => (
+        <div key={projeto.id}>
+          <p>{projeto.nome}</p>
+          <img src={projeto.simbolo} style={{ width: "200px" }} />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+//funçao principal que exporta a pagina 
 function Home() {
-  const home = useState(false)
+  const home = useState(false);
   return (
     <body>
+      <main>
+        {navegacao()}
       <aside>nome e foto</aside>
-      <p>slogan</p>
-      <article >projetos
-        <div></div>
-        <div></div>
-        <div></div>
-      </article>
+      <p></p>
+      {exibeprojeto()}
       <article>skills</article>
       <article>about</article>
       <article>contact</article>
+      </main>
       <footer></footer>
     </body>
-  )
+  );
 }
-export default Home
+export default Home;
