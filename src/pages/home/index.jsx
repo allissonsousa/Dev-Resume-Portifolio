a515cfa4608673a8fde6fc62387c287779c12b3f
Allison de Paulo Sousa Avila <111160403+allissonsousa@users.noreply.github.com>
2025-08-12 23:59:10 -0300

    Move assets to public directory and update imports
    All image assets were relocated from src/assets to public/assets, and all corresponding import paths in home/index.jsx were updated to reflect this change. This improves asset management and ensures correct asset referencing in the application.

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -6,9 +6,9 @@ import { useEffect } from "react";
 
 function exibeprojeto() {
   const projetos = [
-    { id: 1, imagem: "src/assets/analisedados.png", nome: "Saúde Quest", content: "Análise de dados com python usando dados do sistema único de saúde", tags: "Python Pandas AI SQL Excel" },
-    { id: 2, imagem: "src/assets/dados-financeiros.jpeg", nome: "Finance View", content: "Criação de um acompanhador de valores da bolsa atual com Cruds em Java", tags: "JAVA Springboot Spring SQL Docker" },
-    { id: 3, imagem: "src/assets/speed.png", nome: "Speed Timer", content: "Cronometro para cubo mágico com métricas ", tags: "HTML CSS JavaScript " }
+    { id: 1, imagem: "assets/analisedados.png", nome: "Saúde Quest", content: "Análise de dados com python usando dados do sistema único de saúde", tags: "Python Pandas AI SQL Excel" },
+    { id: 2, imagem: "assets/dados-financeiros.jpeg", nome: "Finance View", content: "Criação de um acompanhador de valores da bolsa atual com Cruds em Java", tags: "JAVA Springboot Spring SQL Docker" },
+    { id: 3, imagem: "assets/speed.png", nome: "Speed Timer", content: "Cronometro para cubo mágico com métricas ", tags: "HTML CSS JavaScript " }
   ];
   return (
     <div className="projetos">
@@ -33,7 +33,7 @@ function habilidades() {
   ];
   return (
     <div className="habilidades">
-      <img src="src/assets/skil.png" alt="skilsmold" />
+      <img src="assets/skil.png" alt="skilsmold" />
       <div className="cast">
         {habilidades.map((habilidade) => (
           <aside>
@@ -63,7 +63,7 @@ function Home() {
   return (
     <div id="corpo">
       <header id="cabeçalho">
-        <div><img src="src/assets/selecao.png" alt="simbolo" />Allisson</div>
+        <div><img src="assets/selecao.png" alt="simbolo" />Allisson</div>
         <div id="navegacao">
           <div onClick={() => scrollToSection("Home")}><span>#</span>home</div>
           <div onClick={() => scrollToSection("Projects")}><span>#</span>trabalhos</div>
@@ -73,10 +73,10 @@ function Home() {
         </div>
       </header>
       <div className="contatosfixo" >
-        <img id="linha" src="src/assets/linha.png" alt="linha" />
-        <a href="" target="_blank"><img src="src/assets/discordia.png" alt="discord" /></a>
-        <a href="https://github.com/allissonsousa" target="_blank"><img src="src/assets/github.png" alt="github" /></a>
-        <a href="mailto:allissonsousa7@outlook.com" target="_blank"><img src="src/assets/mail.png" alt="e-mail" /></a>
+        <img id="linha" src="assets/linha.png" alt="linha" />
+        <a href="" target="_blank"><img src="assets/discordia.png" alt="discord" /></a>
+        <a href="https://github.com/allissonsousa" target="_blank"><img src="assets/github.png" alt="github" /></a>
+        <a href="mailto:allissonsousa7@outlook.com" target="_blank"><img src="assets/mail.png" alt="e-mail" /></a>
       </div>
       <main ref={sectionsRef.Home}>
         <aside className="sobremim" >
@@ -86,31 +86,31 @@ function Home() {
             <div id="contateme">Contate-me!!</div>
           </article>
           <article className="usuario">
-            <img id="simbolofoto" src="src/assets/simbol.png" alt="simbolo" />
-            <img id="foto" src="src/assets/perfil.png" alt="perfil" />
+            <img id="simbolofoto" src="assets/simbol.png" alt="simbolo" />
+            <img id="foto" src="assets/perfil.png" alt="perfil" />
             <p id="workingon"><span>#</span>Atualmente -- estudando JAVA</p>
-            <img id="pontofoto" src="/src/assets/malha.png" alt="pontos" />
+            <img id="pontofoto" src="assets/malha.png" alt="pontos" />
           </article>
         </aside>
         <aside className="slogan">
-          <img className="aspasinvertido rodaraspas" src="src/assets/aspas.png" alt="aspas" />
+          <img className="aspasinvertido rodaraspas" src="assets/aspas.png" alt="aspas" />
           <div >
             <p>"A evolução não é instantânea, mas inevitável quando há curiosidade e persistência."</p>
             <p>— Carl Sagan</p>
           </div>
-          <img ref={sectionsRef.Projects} src="src/assets/aspas.png" alt="aspas" />
+          <img ref={sectionsRef.Projects} src="assets/aspas.png" alt="aspas" />
         </aside>
         <article ref={sectionsRef.Skills}>
-          <h1 className="titulos"><span>#</span>projects <span><img src="src/assets/menos-linha-reta-horizontal.png" alt="linha" /></span></h1>
+          <h1 className="titulos"><span>#</span>projects <span><img src="assets/menos-linha-reta-horizontal.png" alt="linha" /></span></h1>
           {exibeprojeto()}
         </article>
         <article >
-          <h1 className="titulos"><span>#</span>skills <span><img src="src/assets/menos-linha-reta-horizontal.png" alt="linha" /></span></h1>
+          <h1 className="titulos"><span>#</span>skills <span><img src="assets/menos-linha-reta-horizontal.png" alt="linha" /></span></h1>
           {habilidades()}
         </article>
         <article ref={sectionsRef.About} className="sobre">
           <div >
-            <h1 className="titulos"><span>#</span>about-me <span><img src="src/assets/menos-linha-reta-horizontal.png" alt="linha" /></span></h1>
+            <h1 className="titulos"><span>#</span>about-me <span><img src="assets/menos-linha-reta-horizontal.png" alt="linha" /></span></h1>
             <p>Sou um programador em início de carreira com sólida base em linguagens como Python, Java e SQL, além de experiência prática com desenvolvimento
               web utilizando HTML, CSS e React. Já desenvolvi projetos pessoais e acadêmicos que envolvem desde criação de interfaces responsivas até manipulação
               de dados em bancos relacionais. Tenho facilidade para aprender novas tecnologias, foco em entregar código limpo e estou sempre buscando soluções práticas
@@ -121,20 +121,20 @@ function Home() {
               contato.</p>
           </div>
           <div >
-            <img className="perf" src="src/assets/aboutperfil.png" alt="pontos" />
+            <img className="perf" src="assets/aboutperfil.png" alt="pontos" />
           </div>
         </article>
         <article ref={sectionsRef.Contact}>
-          <h1 className="titulos"><span>#</span>contacts <span><img src="src/assets/menos-linha-reta-horizontal.png" alt="linha" /></span></h1>
+          <h1 className="titulos"><span>#</span>contacts <span><img src="assets/menos-linha-reta-horizontal.png" alt="linha" /></span></h1>
           <div>
             <p>Estou aberto a novas oportunidades como programador, sejam projetos pontuais ou posições efetivas.</p>
             <p>Sinta-se à vontade para me contatar ou conhecer mais através das minhas redes sociais.</p>
           </div>
           <div>
             <p>Me envie uma mensagem!!</p>
-            <a href="https://www.linkedin.com/in/allisson-sousa-7b8b1b1b1/" target="_blank"><img src="src/assets/linkedin.png" alt="linkedin" /></a>
-            <a href="mailto:allissonsousa7@outlook.com" target="_blank"><img src="src/assets/mail.png" alt="mail" /></a>
-            <a href="" target="_blank"><img src="src/assets/discordia.png" alt="discord" /></a>
+            <a href="https://www.linkedin.com/in/allisson-sousa-7b8b1b1b1/" target="_blank"><img src="assets/linkedin.png" alt="linkedin" /></a>
+            <a href="mailto:allissonsousa7@outlook.com" target="_blank"><img src="assets/mail.png" alt="mail" /></a>
+            <a href="" target="_blank"><img src="assets/discordia.png" alt="discord" /></a>
           </div>
         </article>
       </main>
